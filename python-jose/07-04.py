ab9285d4e58487559a4d949238b3ad0dba5db557
duarte-felipe santos <felpsduarte055@gmail.com>
2025-04-14 19:36:31 +0000

    novo

--- python-jose/07-04.py
+++ python-jose/07-04.py
@@ -7,8 +7,17 @@ if voce == "nao":
 else:
     print("UE...?????") '''
 
+'''tabuada
+num1= int(input("Digite um numero: "))
+tabuada=[num1*1,num1*2,num1*3,num1*4,num1*5,num1*6,num1*7,num1*8,num1*9,num1*10]
+for num1 in tabuada:
+    print(num1)'''
 
-#licao1
+
+
+
+
+#licao6
 def verificar_positivo(numero):
     if numero > 0:
         print("O número é positivo.")
@@ -17,7 +26,7 @@ def verificar_positivo(numero):
 numero = int(input("Digite um número: "))
 verificar_positivo(numero)
 
-#licao2
+#licao7
 def verificar_par_impar(numero):
     if numero % 2 == 0:
         print("O número é par.")
@@ -26,7 +35,7 @@ def verificar_par_impar(numero):
 numero = int(input("Digite um número: "))
 verificar_par_impar(numero)
 
-#licao3
+#licao8
 def ordem_decrescente(valor1, valor2):
     if valor1 > valor2:
         print(f"{valor1} > {valor2}")
@@ -40,8 +49,7 @@ ordem_decrescente(valor1, valor2)
 
 
 
-#tabuada
-num1= int(input("Digite um numero: "))
-tabuada=[num1*1,num1*2,num1*3,num1*4,num1*5,num1*6,num1*7,num1*8,num1*9,num1*10]
-for num1 in tabuada:
-    print(num1)
+
+
+
+
---
+++ python-jose/14-04.py
@@ -0,0 +1,102 @@
+
+'''import numpy 
+
+total = arr.sum()
+media = np.avenrage(arr)
+
+print('\na soma é: ',total)
+print('\nA media é: ', media)'''
+
+
+#licao9
+def multiplosde_3():
+    soma = 0
+    for i in range(1, 501):
+        if i % 2 != 0 and i % 3 == 0:
+            soma += i
+    return soma
+
+result = multiplosde_3()
+print("A soma dos impares multiplos de 3 de 1 até 500 é:", result)
+
+
+#licao10 
+def contagem_regressiva():
+    min = 10
+    seg = 0
+    while min >= 0:
+        print(f"{min:02d}:{seg:02d}")
+        if seg == 0:
+            min -= 1
+            seg = 59
+        else:
+            seg -= 1
+        if min == 8 and seg == 57:  #loop infinito
+            break
+
+contagem_regressiva()
+
+#licao11
+def tabuada_do_5():
+    for i in range(1, 11):
+        print(f"5 x {i} = {5 * i}")
+
+tabuada_do_5()
+
+#licao12
+def pares():
+    pares = []
+    for i in range(1, 101):
+        if i % 2 == 0:
+            pares.append(i)
+    return pares
+
+result = pares()
+print("Os numeros pares de 1 a 100 são:", result)
+
+#licao13
+def fatorial(num):
+    resultado = 1
+    for i in range(1, num + 1):
+        resultado *= i
+    return resultado
+
+num = int(input("Digite um numero que vou calcular o fatorial: "))
+result = fatorial(num)
+print(f"O fatorial de {num} é {result}.")
+
+#licao14
+def divisiveis_7():
+    cont = 0
+    for i in range(1, 1001):
+        if i % 7 == 0:
+            cont += 1
+    return cont
+
+result = divisiveis_7()
+print("A quantidade de numeros entre 1 e 1000 divisiveis por 7 são:", result)
+
+#licao15
+def media(elementos):
+    return sim(elementos) / len(elementos)
+
+    print(media([10,20,30]))
+
+#licao16
+'''lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print (lista ('1,3,5,7,9,11,13,15'))'''
+
+#licao17
+def maior_menor():
+    lista = [10, 5, 23, 7, 15, 1, 99, 32]
+  
+    maior = max(lista)
+    menor = min(lista)
+    return maior, menor
+maior_valor, menor_valor = maior_menor()
+print(f"Maior valor: {maior_valor}")
+print(f"Menor valor: {menor_valor}")
+
+#licao18
+def somar(lista1, lista2):
+    min_len = min(len(lista1[i]/))
